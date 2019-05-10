e5494117656ce549ea12f612e59925dd14f3a235
Amy Quinn <amyequinn@hotmail.co.uk>
2019-05-10 11:47:58 +0100

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,16 +1,9 @@
-require "pry"
-
-birthday_kids = {
-    "Timmy" => 9,
-    "Sarah" => 6,
-    "Amanda" => 27
-}
-
 def happy_birthday(birthday_kids)
-birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+    birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
-end
+end    
+
 
 def age_appropriate_birthday(birthday_kids)
 
